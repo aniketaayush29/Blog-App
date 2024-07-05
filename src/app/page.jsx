6477a8f8a6e4244d01cfb8c749ab0caaf01210bc
Aniket Aayush <aniketaayush29@gmail.com>
2024-07-06 00:50:49 +0530

    initial commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,28 @@
+import Link from "next/link";
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Link href="/">
+        <h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        quisquam sit odit excepturi ut necessitatibus sed rem similique odio
+        officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        quisquam sit odit excepturi ut necessitatibus sed rem similique odio
+        officiis?
+        </h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        quisquam sit odit excepturi ut necessitatibus sed rem similique odio
+        officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        quisquam sit odit excepturi ut necessitatibus sed rem similique odio
+        officiis?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+        quisquam sit odit excepturi ut necessitatibus sed rem similique odio
+        officiis?
+      </Link>
+    </div>
+  );
 }
