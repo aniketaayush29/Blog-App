977daa33b324c022171ae74bce766a8eccf2a6b4
varinda <varindagoyal844@gmail.com>
2024-07-28 16:22:32 +0530

    Adding responsiveness

--- src/components/authlinks/AuthLinks.jsx
+++ src/components/authlinks/AuthLinks.jsx
@@ -12,6 +12,24 @@ const AuthLinks = () => {
   return (
   <>
   {status === "notauthenticated"?(
+    <Link href = "/login" className = {styles.link}>Login</Link>
+  )  : (
+    <>
+    <Link href = "/write" className = {styles.link}>Write</Link>
+    <span className = {styles.link}>Logout</span>
+    </>
+  )}
+  <div className= {styles.burger} onClick={() =>setOpen(!open) }>
+    <div className = {styles.line}></div>
+    <div className = {styles.line}></div>
+    <div className = {styles.line}></div>
+ </div>
+ {open&&(
+  <div className = {styles.responsiveMenu}>
+    <Link href = "/" >Home</Link>
+    <Link href = "/" >About</Link>
+    <Link href = "/" >Contact</Link>
+    {status === "notauthenticated"?(
     <Link href = "/login">Login</Link>
   )  : (
     <>
@@ -19,13 +37,8 @@ const AuthLinks = () => {
     <span className = {styles.link}>Logout</span>
     </>
   )}
-  <div className= {styles.burger}>
-    <div className = {styles.line}></div>
-    <div className = {styles.line}></div>
-    <div className = {styles.line}></div>
-    
-    
      </div>
+ )}
   </>
   )
 };
