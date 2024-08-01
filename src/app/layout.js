57a8505997214528da53bab91a9457fb431ce0aa
Aniket Aayush <aniketaayush29@gmail.com>
2024-08-02 00:54:05 +0530

    Minor Modifications

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,33 +1,33 @@
-import Navbar from '@/components/navbar/Navbar'
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Footer from '@/components/footer/Footer'
-import { ThemeContextProvider } from '@/context/ThemeContext'
-import ThemeProvider from '@/provider/ThemeProvider'
+import Navbar from "@/components/navbar/Navbar";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Footer from "@/components/footer/Footer";
+import { ThemeContextProvider } from "@/context/ThemeContext";
+import ThemeProvider from "@/provider/ThemeProvider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'The Blog App',
-  description: 'Exceptionally awesome blog app!',
-}
+  title: "The Blog App",
+  description: "Exceptionally awesome blog app!",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <ThemeContextProvider>
-        <ThemeProvider>
-        <div className="container">
-          <div className="wrapper">
-          <Navbar/>
-          {children}
-            <Footer/>
-          </div>
-        </div>
-        </ThemeProvider>
+          <ThemeProvider>
+            <div className="container">
+              <div className="wrapper">
+                <Navbar />
+                {children}
+                <Footer />
+              </div>
+            </div>
+          </ThemeProvider>
         </ThemeContextProvider>
       </body>
     </html>
-  )
+  );
 }
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,32 +1,33 @@
-import React from 'react'
-import styles from "./menu.module.css"
-import Link from 'next/link'
-import Image from 'next/image'
+import React from "react";
+import styles from "./menu.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
-const  Menu = () => {
+const Menu = () => {
   return (
-    <div
-      className = {styles.container}> Menu
-      <h2> {"What's hot"}</h2>
+    <div className={styles.container}>
+      Menu
+      <h2 className={styles.subtitle}> {"What's hot"}</h2>
+      <h1 className={styles.title}>Most Popular</h1>
       <div className={styles.items}>
-      <Link href = "/" className={styles.item}> 
-      <div className = {styles.imageContainer}>
-        <Image src = "/p1.jpeg" alt = "" fill className = {styles.image}/>
+        <Link href="/" className={styles.item}>
+          <div className={styles.imageContainer}>
+            <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+          </div>
+          <div className={styles.textConatiner}>
+            <span className={`${styles.category} ${styles.travel}`}>
+              Travel
+            </span>
+            <h3>Lorem ipsum dolor sit amet.</h3>
+            <div className={styles.detail}>
+              <span className={styles.username}>John Doe</span>
+              <span className={styles.date}>10.03.2024</span>
+            </div>
+          </div>
+        </Link>
       </div>
-      <div  className = {styles.textConatiner}>
-        <span  className = {`${styles.category} ${styles.travel}`}> Travel </span>
-        <h3>Lorem ipsum dolor sit amet.</h3>
-        <div className = {styles.detail}>
-          <span className = {styles.username}>John Doe</span>
-          <span className = {styles.date}>10.03.2024</span>
-        </div>
-      </div>
-     </Link>
-      </div>
-      
     </div>
-  )
-}
+  );
+};
 
-
-export default Menu
+export default Menu;
